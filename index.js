1486d0a7b1e0bd540187c9bca98c04736251ac30
Farhan <farhanahad3@gmail.com>
2021-12-06 01:17:30 +0600

    added

--- index.js
+++ index.js
@@ -35,6 +35,18 @@ async function run(){
           //console.log(result);
           res.json(result)
         })
+
+        app.get('/users/:email', async (req, res) => {
+          const email = req.params.email;
+          const query = { email: email };
+          const user = await usersCollection.findOne(query);
+          let isAdmin = false;
+          if (user?.role === 'admin') {
+              isAdmin = true;
+          }
+          res.json({ admin: isAdmin });
+      })
+      
         app.post('/users',async(req,res)=>{
           const user=req.body;
           const result=await usersCollection.insertOne(user);
@@ -50,6 +62,15 @@ async function run(){
           const result=await usersCollection.updateOne(filter,updateDoc,options);
           res.json(result);
         })
+
+        app.put('/users/admin', async (req, res) => {
+          const user = req.body;
+          console.log('put',user); 
+          const filter={email:user.email};
+          const updateDoc={$set:{role:'admin'}};
+          const result=await usersCollection.updateOne(filter,updateDoc);
+          res.json(result);
+      })
      }
      finally{
           
