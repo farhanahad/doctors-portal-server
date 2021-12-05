bc4c94ce1845a14a253704291282fbdd1c57ea9a
Farhan <farhanahad3@gmail.com>
2021-12-05 20:43:59 +0600

    edited

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ async function run(){
         await client.connect();
         const database=client.db('doctors_portal');
         const appointmentsCollection=database.collection('appointments');
+        const usersCollection=database.collection('users');
 
         app.get('/appointments',async(req,res)=>{
           const email=req.query.email;
@@ -31,8 +32,23 @@ async function run(){
         app.post('/appointments',async(req,res)=>{
           const appointment=req.body;
           const result=await appointmentsCollection.insertOne(appointment);
-          console.log(result);
+          //console.log(result);
           res.json(result)
+        })
+        app.post('/users',async(req,res)=>{
+          const user=req.body;
+          const result=await usersCollection.insertOne(user);
+          console.log(result);
+          res.json(result);
+        })
+
+        app.put('/users',async(req,res)=>{
+          const user=req.body;
+          const filter={email:user.email};
+          const options={upsert:true};
+          const updateDoc={$set:user};
+          const result=await usersCollection.updateOne(filter,updateDoc,options);
+          res.json(result);
         })
      }
      finally{
