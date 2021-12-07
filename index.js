bdb80f84774734c3fee1e3f6f39b55d9d2a0d150
Farhan <farhanahad3@gmail.com>
2021-12-07 09:24:54 +0600

    added

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 const port =process.env.PORT||5000;
 const stripe = require('stripe')(process.env.STRIPE_SECRET)
+const fileUpload = require('express-fileupload');
 
 
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0kwkb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -21,6 +23,7 @@ async function run(){
         const database=client.db('doctors_portal');
         const appointmentsCollection=database.collection('appointments');
         const usersCollection=database.collection('users');
+        const doctorsCollection = database.collection('doctors');
 
         app.get('/appointments',async(req,res)=>{
           const email=req.query.email;
@@ -69,6 +72,37 @@ async function run(){
       const result = await appointmentsCollection.updateOne(filter, updateDoc);
       res.json(result);
   })
+
+   // doctors api
+   app.get('/doctors', async (req, res) => {
+    const cursor = doctorsCollection.find({});
+    const doctors = await cursor.toArray();
+    res.json(doctors);
+});
+
+app.get('/doctors/:id', async (req, res) => {
+    const query = { _id: ObjectId(req.params.id) }
+    const doctor = await doctorsCollection.findOne(query);
+    res.json(doctor);
+});
+
+
+  app.post('/doctors', async (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const pic = req.files.image;
+    const picData = pic.data;
+    const encodedPic = picData.toString('base64');
+    const imageBuffer = Buffer.from(encodedPic, 'base64');
+    const doctor = {
+        name,
+        email,
+        image: imageBuffer
+    }
+    const result = await doctorsCollection.insertOne(doctor);
+    res.json(result);
+})
+
 
         app.post('/users',async(req,res)=>{
           const user=req.body;
